b8b8d7cc5fcbcceb0c15097ccacff1e6177717a8
Aghatinha <aghata.paulo@etec.sp.gov.br>
2023-03-09 15:03:59 -0300

    Colocamos animação

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedStated', [
+      state('defaut', style({ transform: 'rotateY(0)'})),
+      state('rotated', style({ transform: 'rotateY(-360deg)' })),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in')),
+    ]),
+  ],
 })
 export class Tab1Page {
   cara = 'assets/cara.png';
@@ -11,11 +20,18 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
+  state = 'default';
 
   constructor() {}
 
  jogarMoeda(){
-   if (Math.random() < 0.5){
+   this.image = this.logo;
+   this.info = 'Girando...'
+   this.state = this.state === 'default' ? 'rotated' : 'default';
+
+   setTimeout(()=>{
+
+    if (Math.random() < 0.5){
      this.image = this.cara;
      this.info = 'Cara!';
    }else{
@@ -23,7 +39,7 @@ export class Tab1Page {
      this.info = 'Coroa!'
    }
 
+}, 2000);
  }
-
 }
 
