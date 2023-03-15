77e432e4e21bdb2fb3f3a2cce8d7f40c60938b63
Aghatinha <aghata.paulo@etec.sp.gov.br>
2023-03-15 08:01:24 -0300

    .

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,57 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  valor = 1;
+
+  btn1 = 1;
+  btn2 = 3;
+  btn3 = 6;
+  btn4 = 9;
+  btn5 = 12;
+
+  ponto = 0;
+  ponto2 = 0
+
   constructor() {}
+
+  btnAdd1() {
+    this.valor = this.btn1;
+  }
+
+  btnAdd2() {
+    this.valor = this.btn2;
+  }
+
+  btnAdd3() {
+    this.valor = this.btn3;
+  }
+
+  btnAdd4() {
+    this.valor = this.btn4;
+  }
+
+  btnAdd5() {
+    this.valor = this.btn5;
+  }
+
+  addValue() {
+    this.ponto += this.valor;
+  }
+
+  addValue2() {
+    this.ponto2 += this.valor;
+  }
+
+  removeValue() {
+    this.ponto -= this.valor;
+  }
+
+  removeValue2() {
+    this.ponto2 -= this.valor;
+  }
+
+  clean() {
+    console.log("foi")
+  }
 
 }
